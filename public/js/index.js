ee7e017b2475adda60b1e4caa2cf33a6cb40ac56
Raymond Baguma <you@example.com>
2021-01-06 11:04:31 +0100

    draggable card deck implimentatiion

---
+++ public/js/index.js
@@ -0,0 +1,54 @@
+"use strict"
+const draggables = document.querySelectorAll("article");
+const constainers = document.querySelectorAll("section");
+
+draggables.forEach((draggable) => {
+    //dragg starts
+    draggable.addEventListener("dragstart", () => {
+        draggable.classList.add("dragging");
+    });
+
+    //drag stops
+    draggable.addEventListener("dragend", () => {
+        draggable.classList.remove("dragging");
+    });
+});
+
+// determin element being dragged over. 
+constainers.forEach((container) => {
+    container.addEventListener("dragover", (e) => {
+        e.preventDefault();
+        const afterElement = getDragAfterElement(container, e.clientY);
+
+        const draggable = document.querySelector(".dragging");
+
+        if (afterElement == null) {
+
+            container.appendChild(draggable);
+        } else {
+            container.insertBefore(draggable, afterElement)
+        }
+    });
+});
+
+//determin mouse position to drop above elemnt below
+function getDragAfterElement(container, y) {
+    const draggableElements = [
+        ...container.querySelectorAll(".draggable:not(.dragging)"),
+    ];
+
+    return draggableElements.reduce(
+        (clossest, child) => {
+            const box = child.getBoundingClientRect();
+            const offset = y - box.top - box.height / 2;
+
+            if (offset < 0 && offset > clossest.offset) {
+                return { offset: offset, element: child };
+            } else {
+                return clossest;
+            }
+        }, {
+            offset: Number.NEGATIVE_INFINITY,
+        }
+    ).element;
+}
